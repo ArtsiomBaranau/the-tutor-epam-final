221bbcf482f4b21117317fb692f5d2f953680d96
Artsiom Baranau <artsiombaranau@gmail.com>
2021-05-06 01:18:57 +0300

    add index and login controllers tests

--- the-tutor-web/src/main/java/com/gmail/artsiombaranau/thetutor/controllers/IndexController.java
+++ the-tutor-web/src/main/java/com/gmail/artsiombaranau/thetutor/controllers/IndexController.java
@@ -2,12 +2,15 @@ package com.gmail.artsiombaranau.thetutor.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping({"/", "/index"})
 public class IndexController {
 
-    @GetMapping({"", "/", "/index"})
+    @GetMapping
     public String getIndexPage(){
+
         return "index";
     }
 }
--- the-tutor-web/src/main/java/com/gmail/artsiombaranau/thetutor/controllers/LoginController.java
+++ the-tutor-web/src/main/java/com/gmail/artsiombaranau/thetutor/controllers/LoginController.java
@@ -7,11 +7,9 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/login")
 public class LoginController {
 
-
     @GetMapping
     public String loginForm() {
 
         return "login";
     }
-
 }
